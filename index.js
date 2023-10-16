04f502461e94ec95ed0ca46f1dd70d7f29858377
uhpnat <112111112+uhpnat@users.noreply.github.com>
2023-10-17 01:18:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -71,5 +71,5 @@ app.post("/uploads", upload.single("image"), (req, res) => {
 });
 app.get("/view-image/:filename", (req, res) => {
   const { filename } = req.params;
-  res.sendFile(path.join(__dirname, "uploads", filename));
+ res.sendFile(path.join("uploads", filename));
 });
